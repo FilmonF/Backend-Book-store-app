a5f7c64695b3cbd2f9d096ca215418fb392a0ddf
FilmonF <filmonfirezgi@gmail.com>
2025-05-06 16:04:25 +0300

    Add files via upload

---
+++ cloudinary.js
@@ -0,0 +1,10 @@
+import{v2 as cloudinary} from 'cloudinary';
+
+
+cloudinary.config({
+    cloud_name: 'Untitled',
+    api_key: '299665144713761',
+    api_secret: 'sFuE84jGzBSuLIJRqZoPHGF76Jw',
+});
+
+export default cloudinary;
