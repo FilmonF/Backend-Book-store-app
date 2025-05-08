1d44213d604b628a3312bbe6f010fc723aff647d
FilmonF <filmonfirezgi@gmail.com>
2025-05-08 18:46:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import booksRoute from './routes/booksRoute.js';
-import userRoute from './routes/userRoute.js';
+import booksRoute from "./routes/booksRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
+
 const app = express();
-
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://front-end-book-store-app.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
+app.get("/", (req, res) =>
+  res.status(200).send("Welcome to MERN Stack Tutorial!")
+);
+app.use("/books", booksRoute);
+app.use("/user", userRoute);
 
-app.use(cors({
-  origin: 'https://front-end-book-store-app.vercel.app',
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization']
-}));
-
-
-app.get('/', (req, res) => res.status(200).send('Welcome to MERN Stack Tutorial!'));
-app.use('/books', booksRoute);
-app.use('/user', userRoute);
-
-
-const PORT = process.env.PORT || 5555;
 mongoose
   .connect(process.env.MONGO_DB_URL)
   .then(() => {
-    console.log('Connected to database');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    console.log("Connected to database");
+    app.listen(process.env.PORT || 5555, () => {
+      console.log(`Server running on port ${process.env.PORT || 5555}`);
     });
   })
-  .catch(err => console.error('Database connection error:', err.message));
+  .catch((error) => console.error("Database connection error:", error.message));
