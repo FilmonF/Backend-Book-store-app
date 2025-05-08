7d887962d3f4eecbac221e2a85071612c4a1e5ff
FilmonF <filmonfirezgi@gmail.com>
2025-05-08 17:52:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,30 @@ import booksRoute from './routes/booksRoute.js';
 import userRoute from './routes/userRoute.js';
 
 dotenv.config();
-
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+
+
+app.use(cors({
+  origin: 'https://front-end-book-store-app.vercel.app',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+
 
 app.get('/', (req, res) => res.status(200).send('Welcome to MERN Stack Tutorial!'));
 app.use('/books', booksRoute);
 app.use('/user', userRoute);
 
 
-
+const PORT = process.env.PORT || 5555;
 mongoose
   .connect(process.env.MONGO_DB_URL)
   .then(() => {
     console.log('Connected to database');
-    app.listen(process.env.PORT || 5555, () => {
-      console.log(`Server running on port ${process.env.PORT || 5555}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
-  .catch((error) => console.error('Database connection error:', error.message));
+  .catch(err => console.error('Database connection error:', err.message));
